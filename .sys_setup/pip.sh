afc9759df6bc5f33e7fb992107cb18314bc22bef
Dominik Russo <DominikRusso@tutanota.com>
2019-07-14 16:25:57 +0200

    add ggplot

--- .sys_setup/pip.sh
+++ .sys_setup/pip.sh
@@ -1,4 +1,5 @@
 # install general python libraries
+pip3 install --user ggplot
 pip3 install matplotlib
 pip3 install numpy
 pip3 install pendulum
@@ -8,7 +9,7 @@ pip3 install scipy
 # install hacking and CTF libraries
 pip3 install capstone
 pip3 install cryptography
-pip install fake-useragent
+pip3 install fake-useragent
 pip3 install pwntools
 pip3 install ropgadget
 
