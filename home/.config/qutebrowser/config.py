cd578df0ee337f4141721c7a41de3da18e052e40
Dominik Russo <DominikRusso@tutanota.com>
2020-12-01 17:38:56 +0100

    qutebrowser: command mode navigation with vim keys

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+config_dir = str(config.configdir)
+
 c.colors.tabs.bar.bg = "#000"
 c.colors.tabs.even.bg = "#000"
 c.colors.tabs.odd.bg = "#000"
@@ -18,27 +20,27 @@ c.tabs.position = "left"
 c.tabs.show = "multiple"
 c.tabs.title.format = ""
 c.tabs.width = 25
-c.url.start_pages = ['~/.config/qutebrowser/web/startpage.html']
+c.url.start_pages = [config_dir + '/web/startpage.html']
 c.window.hide_decoration = True
 
 config.bind('<Ctrl-m>', 'hint links spawn --detach mpv --force-window=immediate {hint-url}')
-config.bind('e', 'open-editor')
 
-config_dir = str(config.configdir)
+# navigation in command mode using vim keys (like in fzf)
+config.bind('<Ctrl-j>', 'completion-item-focus next', mode='command')
+config.bind('<Ctrl-k>', 'completion-item-focus prev', mode='command')
 
 # `js-whitelist` file goes in same dir as this config file
 # with each url on a separate line and NO trailing newlines
-with open(config_dir + "/js-whitelist") as whitelist:
+with open(config_dir + '/js-whitelist') as whitelist:
     urls = whitelist.readlines()
     for url in urls:
         config.set('content.javascript.enabled', True, url.strip())
 
 # `searchengines` files goes in same dir as this config file
 # with key and value separated by one space and NO trailing newlines
-with open(config_dir + "/searchengines") as searchengines:
+with open(config_dir + '/searchengines') as searchengines:
     dict = {}
     for line in searchengines:
         (key, val) = line.split()
         dict[key] = val
     c.url.searchengines = dict
-
