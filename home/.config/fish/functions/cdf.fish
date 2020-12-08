dbd402e8166372571459204e7902cffa2ff9e99f
Dominik Russo <DominikRusso@tutanota.com>
2020-12-08 15:34:47 +0100

    fish: improve cdf and vf scripts

--- home/.config/fish/functions/cdf.fish
+++ home/.config/fish/functions/cdf.fish
@@ -1,6 +1,6 @@
 # cd with fzf
 function cdf
-    set dir (find $HOME/.config $HOME/repos $HOME/docs $HOME/media $HOME/.local $HOME/downloads -type d | rg -v '.*/\.git/.*' | sed "s|$HOME|~|" | fzf | sed "s|~|$HOME|")
+    set dir (fd . $HOME -t d --hidden --no-ignore | rg -v -e '\.git' \
+            | sed "s|$HOME|~|" | fzf | sed "s|~|$HOME|")
     [ -d "$dir" ] && builtin cd "$dir"
 end
-
--- home/.config/fish/functions/vf.fish
+++ home/.config/fish/functions/vf.fish
@@ -1,6 +1,15 @@
 # find file and open in vim
 function vf
-    set file (find $HOME/.config $HOME/repos $HOME/docs $HOME/.local $HOME/downloads -type f | rg -v '.*/\.git/.*' | sed "s|$HOME|~|" | fzf | sed "s|~|$HOME|")
+    set file (fd . $HOME -t f --hidden --no-ignore \
+            | rg -v \
+                -e '\.git/' \
+                -e '\.jpeg$' \
+                -e '\.jpg$' \
+                -e '\.mkv$' \
+                -e '\.mp3$' \
+                -e '\.mp4$' \
+                -e '\.ogg$' \
+                -e '\.png$' \
+            | sed "s|$HOME|~|" | fzf | sed "s|~|$HOME|")
     [ -n "$file" ] && "$EDITOR" "$file"
 end
-
