2c413e30481a98f8583617dca24222b477b9c780
Dominik Russo <DominikRusso@tutanota.com>
2021-10-22 17:44:08 +0200

    polybar: simplify config and place an instance on every monitor

--- home/.config/bspwm/polybar.sh
+++ home/.config/bspwm/polybar.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# kill all running instances
 killall -q polybar
 
-export POLYBAR_OFFSET_Y=$( echo $( xrdb -q | grep Xft.dpi | cut -f 2 ) / -28 | bc )
-polybar main -c ~/.config/polybar/config.ini &
+# start main bar on every monitor
+for monitor in $(xrandr --query | grep " connected" | cut -d " " -f 1); do
+  MONITOR=$monitor polybar main -c ~/.config/polybar/config.ini &
+done
